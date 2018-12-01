cee93d2297f068484cd05f1fdde58f9d1490d748
victor77dev <victor77.dev@gmail.com>
2018-12-01 01:27:07 +0100

    Add extra fields in sign up form Added fields: email, first name, last name

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import SignUpForm
 
 
 class SignUp(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = SignUpForm
     success_url = reverse_lazy('login')
     template_name = 'signup.html'
